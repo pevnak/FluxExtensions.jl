23c9d1fd0e235b292e0aadc83d2d6d2c04ee3059
Simon Mandlik <simon.mandlik@gmail.com>
2018-07-24 09:51:51 +0200

    added weighted cross entropy, reimplemented with logsumexp trick

--- src/utils.jl
+++ src/utils.jl
@@ -1,16 +1,17 @@
 adapt(T, x::Array) = T.(x)
 adapt(T, m::Flux.Dense) = Flux.Dense(adapt(T,m.W),adapt(T,m.b),m.σ)
 
-function logitcrossentropy(logit, y) 
-  logit = logit .-maximum(logit,1)
-  ypred = logit .- log.( sum( exp.( logit),1))
-  - sum(y .* ypred)/size(logit,2)
-end
+using FluxExtensions: logsumexp
+
+logit_cross_entropy(logit, y) = -sum(y.*(logit.-logsumexp(logit, 1))) / size(logit,2)
+
+weighted_logit_cross_entropy(logit, y::AbstractVector, w) = weighted_logit_cross_entropy(logit, Flux.onehotbatch(y, 1:size(logit,1)), w)
+weighted_logit_cross_entropy(logit, y::AbstractMatrix, w) = -sum(y.*w.*(logit.-logsumexp(logit, 1)))
 
 entropy(x,dim::Int=1) = -mean(sum(x .* log.(x),dim))
 
 function confusion(y,pred,k)
 	c = zeros(Int,k,k)
 	foreach(i -> c[i[1],i[2]] += 1, zip(y,pred))
-	c 
+	c
 end
